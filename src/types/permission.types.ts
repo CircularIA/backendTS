a4a7f62b025b10d3cff6dfea963fb3052f8a1d74
JP Caniguante <juan.caniguante@etpay.com>
2025-03-26 23:10:55 -0300

    feat: added permissions

--- src/types/permission.types.ts
+++ src/types/permission.types.ts
@@ -1,3 +1,5 @@
+import { USER_ROLES } from "@src/middlewares/roles";
+
 const Resources = {
 	USER: "USER",
 	ADMIN: "ADMIN",
@@ -16,22 +18,71 @@ const Actions = {
 	READ: "READ",
 	UPDATE: "UPDATE",
 	DELETE: "DELETE",
+	MANAGE: "MANAGE", //Action that resumes all the actions above a resource
 };
 
 const Divider = ":";
 
-// type ResourceType = keyof typeof Resources;
+export type IndicatorsType = "ECONOMIC" | "AMBIENTAL" | "SOCIAL";
+
+// export type ResourceType = keyof typeof Resources;
+
 // type ActionType = keyof typeof Actions;
+export type RoleType = keyof typeof USER_ROLES;
 
 const generatePermission = (): string[] => {
 	const permissions: string[] = [];
-
-	Object.values(Resources).forEach((resource) => {
-		Object.values(Actions).forEach((action) => {
-			permissions.push(`${resource}${Divider}${action}`);
+	Object.keys(Resources).forEach((resource) => {
+		Object.keys(Actions).forEach((action) => {
+			permissions.push(`${resource}${Divider}${action}${Divider}`);
 		});
 	});
 	return permissions;
 };
 
-export const PERMISSION = generatePermission();
+const generateAdminPermission = (): string[] => {
+	return [
+		`${Resources.ENTERPRISES}${Divider}${Actions.READ}${Divider}`,
+		`${Resources.ENTERPRISES}${Divider}${Actions.UPDATE}${Divider}`,
+		`${Resources.BRANCHES}${Divider}${Actions.CREATE}${Divider}`,
+		`${Resources.BRANCHES}${Divider}${Actions.READ}${Divider}`,
+		`${Resources.BRANCHES}${Divider}${Actions.UPDATE}${Divider}`,
+		`${Resources.USER}${Divider}${Actions.MANAGE}${Divider}`,
+		`${Resources.INDICATORS}${Divider}${Actions.MANAGE}${Divider}`,
+		`${Resources.INPUT_DATS}${Divider}${Actions.MANAGE}${Divider}`,
+		`${Resources.ECONOMIC_INDICATORS}${Divider}${Actions.MANAGE}${Divider}`,
+		`${Resources.AMBIENTAL_INDICATORS}${Divider}${Actions.MANAGE}${Divider}`,
+		`${Resources.SOCIAL_INDICATORS}${Divider}${Actions.MANAGE}${Divider}`,
+	];
+};
+
+const generateUserPermission = (): string[] => {
+	return [
+		`${Resources.INDICATORS}${Divider}${Actions.READ}${Divider}`,
+		`${Resources.INPUT_DATS}${Divider}${Actions.READ}${Divider}`,
+		`${Resources.INPUT_DATS}${Divider}${Actions.CREATE}${Divider}`,
+		`${Resources.INPUT_DATS}${Divider}${Actions.UPDATE}${Divider}`,
+	];
+};
+
+const generateUserPermissionByTheme = (theme: IndicatorsType): string[] => {
+	return [
+		`${Resources[`${theme}_INDICATORS`]}${Divider}${
+			Actions.MANAGE
+		}${Divider}`,
+	];
+};
+
+//Admin can handle the actions above his enterprise
+const RolePermissions = {
+	SUPER_ADMIN: generatePermission(),
+	ADMIN: generateAdminPermission(),
+	USER: generateUserPermission(),
+	ECONOMIC_USER: generateUserPermissionByTheme("ECONOMIC"),
+	AMBIENTAL_USER: generateUserPermissionByTheme("AMBIENTAL"),
+	SOCIAL_USER: generateUserPermissionByTheme("SOCIAL"),
+};
+
+export const generatePermissionByRole = (role: RoleType): string[] => {
+	return RolePermissions[role];
+};
