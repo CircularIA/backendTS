2eb823770c25aa5ae5de7df7353cdaef7b464d9f
JP Caniguante <juan.caniguante@etpay.com>
2025-05-13 22:00:05 -0400

    feat: added local host route

--- src/config/corsConfigure.ts
+++ src/config/corsConfigure.ts
@@ -1,5 +1,5 @@
 export const corsConfigure = {
-	origin: "http://localhost:3000",
+	origin: ["http://localhost:3000", "http://localhost:5173"],
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	preflightContinue: false,
 	optionsSuccessStatus: 204,
