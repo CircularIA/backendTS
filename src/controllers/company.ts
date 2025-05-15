78d00bf191a2a4be1c73d6006e63cad0e47d08cf
JP Caniguante <juan.caniguante@etpay.com>
2025-05-14 20:39:39 -0400

    feat: added controller

--- src/controllers/company.ts
+++ src/controllers/company.ts
@@ -6,10 +6,13 @@ export const getCompanies = async (
 	res: Response
 ): Promise<Response> => {
 	try {
-		const { user } = req.user;
+		console.log("entro en el controlador");
+		const user = req.user;
+		console.log("user", user);
 		const company = await getCompany(user);
 		return res.status(200).json(company);
 	} catch (error) {
+		console.log("error in getCompanies controller", error);
 		return res.status(500).json({ message: "Internal server error" });
 	}
 };
