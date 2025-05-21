57e18cc16fcb664481c42fb43bfe03769572c197
JP Caniguante <juan.caniguante@etpay.com>
2025-05-21 17:34:48 -0400

    feat: added routes of branch and company

--- src/routes/branch.routes.ts
+++ src/routes/branch.routes.ts
@@ -1,8 +1,22 @@
 import { getBranches } from "@src/controllers/branch";
+import { verifyToken } from "@src/middlewares/auth";
+import { checkPermission } from "@src/middlewares/checkPermission";
+import { authorizeRoles, USER_ROLES } from "@src/middlewares/roles";
+import {
+	Actions,
+	createFormatPermission,
+	Resources,
+} from "@src/types/permission.types";
 import { Router } from "express";
 
 const branchRouter = Router();
 
-branchRouter.get("/", getBranches);
+branchRouter.get(
+	"/",
+	verifyToken,
+	authorizeRoles(USER_ROLES.USER),
+	checkPermission(createFormatPermission(Resources.BRANCHES, Actions.READ)),
+	getBranches
+);
 
 export default branchRouter;
--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -1,9 +1,22 @@
 import { getCompanies } from "@src/controllers/company";
 import { verifyToken } from "@src/middlewares/auth";
+import { checkPermission } from "@src/middlewares/checkPermission";
+import { authorizeRoles, USER_ROLES } from "@src/middlewares/roles";
+import {
+	Actions,
+	createFormatPermission,
+	Resources,
+} from "@src/types/permission.types";
 import { Router } from "express";
 
 const companyRouter = Router();
 
-companyRouter.get("/", verifyToken, getCompanies);
+companyRouter.get(
+	"/",
+	verifyToken,
+	authorizeRoles(USER_ROLES.USER),
+	checkPermission(createFormatPermission(Resources.BRANCHES, Actions.READ)),
+	getCompanies
+);
 
 export default companyRouter;
