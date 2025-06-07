432e3a63ab313448a90c5568e942a36fb010cfa6
JP Caniguante <juan.caniguante@etpay.com>
2025-06-07 12:18:11 -0400

    feat: added indicatorn routes

--- src/routes/indicator.routes.ts
+++ src/routes/indicator.routes.ts
@@ -4,17 +4,34 @@ import {
 	getIndicatorValue,
 } from "@src/controllers/indicator";
 import { verifyToken } from "@src/middlewares/auth";
+import { checkPermission } from "@src/middlewares/checkPermission";
+import {
+	Actions,
+	createFormatPermission,
+	Resources,
+} from "@src/types/permission.types";
 import { Router } from "express";
 
 const indicatorRouter = Router();
 
-indicatorRouter.get("/:indicatorId", verifyToken, getIndicatorInfo);
+indicatorRouter.get(
+	"/:indicatorId",
+	verifyToken,
+	checkPermission(createFormatPermission(Resources.INDICATORS, Actions.READ)),
+	getIndicatorInfo
+);
 
-indicatorRouter.get("/:branchId", verifyToken, getIndicators);
+indicatorRouter.get(
+	"/:branchId",
+	verifyToken,
+	checkPermission(createFormatPermission(Resources.INDICATORS, Actions.READ)),
+	getIndicators
+);
 
 indicatorRouter.get(
 	"/values/:branch/:indicator/:year/:month?",
 	verifyToken,
+	checkPermission(createFormatPermission(Resources.INDICATORS, Actions.READ)),
 	getIndicatorValue
 );
 
