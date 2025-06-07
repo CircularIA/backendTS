7f5393b8fe818554f71644a273ebb293ebed3c42
JP Caniguante <juan.caniguante@etpay.com>
2025-06-07 12:17:10 -0400

    feat: added the new schema in controllers

--- src/controllers/branch.ts
+++ src/controllers/branch.ts
@@ -5,11 +5,7 @@ export const getBranches = async (
 	req: Request,
 	res: Response
 ): Promise<Response> => {
-	try {
-		const { user } = req.user;
-		const branches = await getBranch(user._id);
-		return res.status(200).json(branches);
-	} catch (error) {
-		return res.status(500).json({ message: "Internal Server Error" });
-	}
+	const { user } = req.user;
+	const branches = await getBranch(user._id);
+	return res.status(200).json(branches);
 };
