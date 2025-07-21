a0a4914fd6213f7863bdd79d9d0099b4cf426615
JP Caniguante <juan.caniguante@etpay.com>
2025-07-20 21:48:24 -0400

    feat: aded the fields of the emission source

--- src/models/EmissionSource.ts
+++ src/models/EmissionSource.ts
@@ -1,12 +1,26 @@
+import { Measurements } from "@src/types/common.types";
+import {
+	SourceEmissionDirect,
+	SourceEmissionIndirect,
+	SourceEmissionOther,
+	TypeEmission,
+} from "@src/types/emissionSource.types";
 import { model, Schema, Types } from "mongoose";
 
 interface IEmissionSource extends Document {
 	_id: Types.ObjectId;
-	type_emission: 0;
-	name: string;
-	description: string;
+	type_emission: TypeEmission;
+	source_emission:
+		| SourceEmissionDirect
+		| SourceEmissionIndirect
+		| SourceEmissionOther;
+	detail_emission: string;
+	//Info of the company and the branch
 	company: Types.ObjectId;
 	branch: Types.ObjectId;
+	user: Types.ObjectId;
+	//Info of the foot print numbers
+	measurement: Measurements;
 	createdAt: Date;
 	updatedAt: Date;
 }
