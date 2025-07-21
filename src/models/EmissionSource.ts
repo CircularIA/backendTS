b3d8721f04bfa712497aa0e4b5e7ffa866f96470
JP Caniguante <juan.caniguante@etpay.com>
2025-07-21 17:47:26 -0400

    feat: added emission source models

--- src/models/EmissionSource.ts
+++ src/models/EmissionSource.ts
@@ -21,6 +21,41 @@ interface IEmissionSource extends Document {
 	user: Types.ObjectId;
 	//Info of the foot print numbers
 	measurement: Measurements;
-	createdAt: Date;
-	updatedAt: Date;
+	quantity: number;
+	//Files of evidence
+	files: File[] | File;
+	//Metrics to calculate the footprint
+	emission_factor: number;
+	unit_factor: string;
+	origin_factor: string;
 }
+
+const emissionSourceSchema = new Schema<IEmissionSource>(
+	{
+		_id: Schema.Types.ObjectId,
+		type_emission: { type: String, required: true },
+		source_emission: { type: String, required: true },
+		detail_emission: { type: String, required: true },
+		company: {
+			type: Schema.Types.ObjectId,
+			ref: "Company",
+			required: true,
+		},
+		branch: { type: Schema.Types.ObjectId, ref: "Branch", required: true },
+		user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		measurement: { type: String, required: true },
+		quantity: { type: Number, required: true },
+		files: { type: [Schema.Types.Mixed], required: true },
+		emission_factor: { type: Number, required: true },
+		unit_factor: { type: String, required: true },
+		origin_factor: { type: String },
+	},
+	{ timestamps: true }
+);
+
+const EmissionSourceModel = model<IEmissionSource>(
+	"EmissionSource",
+	emissionSourceSchema
+);
+
+export default EmissionSourceModel;
