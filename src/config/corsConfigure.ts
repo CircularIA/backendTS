e75c9f71414c9298bf8f341770dfebcdb40209a8
JP Caniguante <juan.caniguante@etpay.com>
2025-09-10 08:43:40 -0300

    Revert "fix: allow origins"
    This reverts commit eec9a1482f5693a582695f0fe542c259337ca501.

--- src/config/corsConfigure.ts
+++ src/config/corsConfigure.ts
@@ -1,10 +1,9 @@
 export const corsConfigure = {
-	// origin: process.env.CORS_ORIGIN,
-	origin: "*",
+	origin: process.env.CORS_ORIGIN,
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	preflightContinue: false,
 	optionsSuccessStatus: 204,
-	credentials: false,
+	credentials: true,
 	allowedHeaders: "Content-Type, Authorization, X-Requested-With, Accept",
 	exposedHeaders: "Content-Type, Authorization, X-Requested-With, Accept",
 	maxAge: 86400,
