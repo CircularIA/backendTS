eec9a1482f5693a582695f0fe542c259337ca501
JP Caniguante <juan.caniguante@etpay.com>
2025-09-10 08:38:26 -0300

    fix: allow origins

--- src/config/corsConfigure.ts
+++ src/config/corsConfigure.ts
@@ -1,9 +1,10 @@
 export const corsConfigure = {
-	origin: process.env.CORS_ORIGIN,
+	// origin: process.env.CORS_ORIGIN,
+	origin: "*",
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	preflightContinue: false,
 	optionsSuccessStatus: 204,
-	credentials: true,
+	credentials: false,
 	allowedHeaders: "Content-Type, Authorization, X-Requested-With, Accept",
 	exposedHeaders: "Content-Type, Authorization, X-Requested-With, Accept",
 	maxAge: 86400,
